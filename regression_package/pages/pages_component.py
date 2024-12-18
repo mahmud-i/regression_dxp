85d827e024d20817436b6581098860c9f29b59d3
mahmud-i <rabby2515@example.com>
2024-12-18 10:29:04 +0600

    Revert "Update on spacing"
    This reverts commit 9e5e3a8ba11cd6441a99418a301d10b69a52cde6.

--- regression_package/pages/pages_component.py
+++ regression_package/pages/pages_component.py
@@ -52,6 +52,7 @@ class PageInstance:
             print(f"Error getting attribute '{attribute_name}': {e}")
             return None
 
+
     def wait_for_page_load(self):
         try:
             self.page.wait_for_load_state('networkidle')
@@ -81,6 +82,7 @@ class PageInstance:
     def close_page(self):
         self.page.close()
 
+
     def get_images_data(self):
         try:
             """
@@ -107,7 +109,7 @@ class PageInstance:
                 img_name = os.path.basename(img_url) if img_url else None
 
                 # Include image data only if it is from the specified domain
-                # if domain == 'images.ctfassets.net':
+                #if domain == 'images.ctfassets.net':
                 images.append({
                     "image url": img_url,
                     "image name": img_name,
@@ -116,6 +118,7 @@ class PageInstance:
             return images
         except Exception as e:
             print(f"image found error {e}")
+
 
     def get_page_type(self):
         try:
